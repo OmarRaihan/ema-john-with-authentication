c4b9829c75f15c60d0990493a45445be8632eb59
OmarRaihan <omarraihan91@gmail.com>
2022-04-10 22:08:21 +0600

    updated shipping form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,52 +1,50 @@
-
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import './SignUP.css'
-import auth from '../../firebase.init';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import "./SignUP.css";
+import auth from "../../firebase.init";
 
 const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth)
+  const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const handleEmailBlur = event =>{
-        setEmail(event.target.value);
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordBlur = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  if (user) {
+    navigate("/shop");
+  }
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Your two password did not match");
+      return;
+    }
+    if (password.length < 6) {
+      setError("Password must be 6 characters or longer");
+      return;
     }
 
-    const handlePasswordBlur = event =>{
-        setPassword(event.target.value)
-    }
+    createUserWithEmailAndPassword(email, password);
+  };
 
-    const handleConfirmPasswordBlur = event =>{
-        setConfirmPassword(event.target.value)
-    }
-
-    if(user){
-      navigate('/shop');
-    }
-
-    const handleCreateUser = event =>{
-        event.preventDefault();
-        if(password !== confirmPassword){
-            setError('Your two password did not match');
-            return;
-        }
-        if(password.length <6){
-          setError('Password must be 6 characters or longer');
-          return;
-        }
-
-        createUserWithEmailAndPassword(email, password);
-    }
-
-    return (
-        <div className="form-container">
+  return (
+    <div className="form-container">
       <div>
         <h2 className="form-title">Sign Up</h2>
         <form onSubmit={handleCreateUser}>
@@ -62,15 +60,18 @@ const SignUp = () => {
             <label htmlFor="confirm-password">Confirm Password</label>
             <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
           </div>
-          <p style={{color: 'red'}}>{error}</p>
+          <p style={{ color: "red" }}>{error}</p>
           <input className="form-submit" type="submit" value="Sign Up" />
         </form>
         <p>
-            Already have an account? <Link className="form-link" to="/login">Login</Link>
+          Already have an account?{" "}
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
         </p>
       </div>
     </div>
-    );
+  );
 };
 
 export default SignUp;
